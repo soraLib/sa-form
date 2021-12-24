05db07498c4a2e63b8ae5d8d2a0e1e7f0525f246
童锴 <346762712@qq.com>
2021-12-24 09:48:19 +0800

    feat: build:doc script

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()]
+  plugins: [vue(), vueJsx()],
+  build: {
+    outDir: process.env.BUILD_ENV === 'doc' ? 'docs' : 'dist'
+  }
 });
