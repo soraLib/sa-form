b8d9ad1b3ecc26cf2b99d20fafc53669c282aaf5
hfutsora <31959015+Hfutsora@users.noreply.github.com>
2021-12-24 16:22:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: '/sa-form/',
   build: {
     outDir: process.env.BUILD_ENV === 'doc' ? 'docs' : 'dist'
   }
