a1e8196d9214e119ced9528b98f3643b4ed4d578
童锴 <346762712@qq.com>
2021-12-27 15:09:57 +0800

    feat: add node contextmenu event

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import './index.css';
 import './components/VContextmenu/themes/default/index.scss';
+import 'element-plus/dist/index.css';
+import './index.css';
 
+import ElementPlus from 'element-plus';
 import contextmenu from './components/VContextmenu/index';
 
 const app = createApp(App);
 app.use(contextmenu);
+app.use(ElementPlus);
 
 app.mount('#app');
