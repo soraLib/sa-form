b3712114820996a5a96d0bf039e2dbe594e883d8
童锴 <346762712@qq.com>
2022-06-07 17:56:09 +0800

    chore: import

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import App from './App.vue';
 
 import './components/VContextmenu/themes/default/index.scss';
 import 'element-plus/dist/index.css';
+// import 'element-plus/theme-chalk/dark/css-vars.css'; TODO: dark mode
 import './index.css';
 import './assets/iconfont.css';
 
 import contextmenu from './components/VContextmenu/index';
-
 import dialogDrag from './directives/dialog-drag';
 
 const app = createApp(App);
