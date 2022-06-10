0882f1f23d2bc8eb34bb27964b65101f4e5da30b
hfutsora <346762712@qq.com>
2022-06-10 10:52:42 +0800

    style: layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 
 <template>
-  <div class="w-3/5 mx-auto h-full" style="min-width: calc(472px + 0.5rem);">
-
-    <header class="flex justify-center items-center">
-      <h1 class="font-bold text-lg py-4">Sa Form</h1>
-    </header>
-
-    <SaForm id="1" type="PcForm" class="h-1/2" />
+  <div class="w-3/5 mx-auto h-full flex" style="min-width: calc(472px + 0.5rem);">
+    <SaHeader />
+    <SaForm id="1" type="PcForm" class="h-3/4 m-auto" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import SaForm from './SaForm/index';
+import SaHeader from './components/Header'
+import SaForm from './SaForm/index'
+
 </script>
 
 <style>
@@ -22,6 +20,9 @@ import SaForm from './SaForm/index';
   text-align: center;
   color: #2c3e50;
   height: 100vh;
+  width: 100vh;
+  padding-top: 3.6rem;
+  box-sizing: border-box;
 }
 
 body {
