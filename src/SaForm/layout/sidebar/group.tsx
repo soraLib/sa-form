7e1544a9532e14e5c2a67f42617a3ad5fbd29c27
hfutsora <346762712@qq.com>
2023-02-10 18:04:41 +0800

    fix: native item key

--- src/SaForm/layout/sidebar/group.tsx
+++ src/SaForm/layout/sidebar/group.tsx
@@ -20,6 +20,7 @@ export default defineComponent({
 
     return () => (
       <div class={useClazs('stencil-group', { collapsed: collapsed.value })}>
+
         <div class="stencil-group__header select-none" onClick={() => toggleGroupCollapse()}>
           <NIcon class="caret" size={12} component={CaretDown} />
           <span class="name">{props.group[0]}</span>
@@ -27,7 +28,7 @@ export default defineComponent({
 
         <div class="stencil-group__content">
           {
-            props.group[1].map(item => <NativeItem item={item} />)
+            props.group[1].map(item => <NativeItem key={item.attrs.name} item={item} />)
           }
         </div>
       </div>
